b26e84723b517db3ce4a06cea7060beb56362c13
Stéphane Liem Nguyen <stephane.nguyen@etu.unige.ch>
2023-04-21 17:06:41 +0200

    all results plotted

--- presentations/presentation_3/python-codes/read_all_res.py
+++ presentations/presentation_3/python-codes/read_all_res.py
@@ -7,12 +7,14 @@ Created on Fri Apr 21 08:50:09 2023
 import numpy as np
 import pandas as pd
 
-if __name__ == "__main__":
-    df = pd.read_csv("all_res.csv", header=None, names=range(3))
-    table_names = ["Rmax=? [ F is_done=1 ]",
-                   "Rmin=? [ F is_done=1 ]",
-                   'R{"steps"}max=? [ F is_done=1 ]',
-                   'R{"steps"}min=? [ F is_done=1 ]']
+
+def read_all_res(filename="all_res.csv",
+                 properties_path="../properties.props"):
+    df = pd.read_csv(filename, header=None, names=range(3))
+    with open(properties_path, "r") as f:
+        table_names = list(line for line in (l.strip() for l in f) if line)
+
     groups = df[0].isin(table_names).cumsum()
-    tables = {g.iloc[0,0]: (g.iloc[1:].rename(columns=g.iloc[1])).iloc[1:]
+    tables = {g.iloc[0,0]: ((g.iloc[1:].rename(columns=g.iloc[1].str.strip())).iloc[1:]).astype(float)
      for k,g in df.groupby(groups)}
+    return tables
--- presentations/presentation_3/python-codes/surface_plot_results.py
+++ presentations/presentation_3/python-codes/surface_plot_results.py
@@ -8,19 +8,50 @@ Importing exported results from PRISM and surface plot
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+from read_all_res import read_all_res
 
-
-if __name__ == "__main__":
-    df = pd.read_csv("./prism_results_prior_to_tansen_modif.csv", skipinitialspace=True)
+def plot_res(df):
+    # df = pd.read_csv("./prism_results_prior_to_tansen_modif.csv", skipinitialspace=True)
     X = df["p_bar"].values.reshape((11, 11))
     Y = df["v_init"].values.reshape((11, 11))
     Z = df["Result"].values.reshape((11, 11))
     
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     surf = ax.plot_surface(X, Y, Z, cmap="coolwarm",
-                       linewidth=0, antialiased=False)
+                        linewidth=0, antialiased=False)
     plt.xlabel("probability of choosing to bar: p_bar")
     plt.ylabel("initial value of the shares: v_init")
     ax.set_zlabel("maximum reward")
     plt.savefig("./prism_results.PNG")
+
+def plot_all_res(tables):
+    fig, axs = plt.subplots(2, len(tables)//2,
+                            figsize=(7, 7),
+                            subplot_kw={"projection": "3d"})
+    i= 0
+    for k, data in tables.items():
+        print(2*i//len(tables), i % (len(tables)//2))
+        ax = axs[2*i//len(tables), i % (len(tables)//2)]
+        
+        df = data
+        X = df["p_bar"].values.reshape((11, 11))
+        Y = df["v_init"].values.reshape((11, 11))
+        Z = df["Result"].values.reshape((11, 11))
+        
+        surf = ax.plot_surface(X, Y, Z, cmap="coolwarm",
+                           linewidth=0, antialiased=False)
+        ax.set_xlabel("p_bar")
+        ax.set_ylabel("v_init")
+        # ax.set_zlabel(k)
+        ax.set_title(k)
+        
+        i+=1
+    plt.suptitle("Some quantitive properties with:\n"+\
+                 "p_bar=0:0.1:1, interest=0,\nv_init=0:1:10, tmax=12 months\n")
+    plt.gcf().subplots_adjust(top=0.85)
+    plt.savefig("./all_res.PNG")
+
+if __name__ == "__main__":
     
+    tables = read_all_res()
+    plot_all_res(tables)
